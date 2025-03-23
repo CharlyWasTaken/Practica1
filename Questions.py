781964e6bc49a5231e4eb28e51b82c5f14a3427f
Benjamin <cardozobenja99@gmail.com>
2025-03-23 20:39:50 -0300

    Arreglos Del Punto 1, y Punto 3

--- Questions.py
+++ Questions.py
@@ -22,13 +22,11 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+for Pregunta,respuestas,RespuestaCorrecta in questions_to_ask:
+    print(Pregunta)
+    for i, answer in enumerate(respuestas):
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -37,18 +35,18 @@ for _ in range(3):
             print("Respuesta no Valida")
             exit(1)
         #   Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == RespuestaCorrecta:
             Puntuacion+=1
             print("¡Correcto!")
             break
         else:
-            Puntuacion-=1
+            Puntuacion-=0.5
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-        [correct_answers_index[question_index]])
+        print(respuestas[RespuestaCorrecta])
 # Se imprime un blanco al final de la pregunta
+print(Puntuacion)
 print()
 
